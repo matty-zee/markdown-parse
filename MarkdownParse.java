6bb4328016b20e1a56700ea436829313714d4ef7
matty-zee <97648068+matty-zee@users.noreply.github.com>
2022-01-26 19:13:10 -0800

    Fixing file path strings

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,7 +11,6 @@ public class MarkdownParse {
         // the next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
-            System.out.println(currentIndex);
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
