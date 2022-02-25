f0264d669a5a95643cf05f779a8eeb9571e92635
matty-zee <97648068+matty-zee@users.noreply.github.com>
2022-02-24 18:23:26 -0800

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -3,74 +3,37 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Stack;
 
 public class MarkdownParse {
-
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
-        
+        // find the next [, then find the ], then find the (,
+        // then take up to the next )
         int currentIndex = 0;
-        Stack<Character> bracketTracker = new Stack<>(); 
-        boolean findLink = false;
-        int start = 0;
-        int end = 0;
-        while (currentIndex < markdown.length()) {
-            char curr = markdown.charAt(currentIndex);
-            //if an escape char is found, skip it and the 
-            //character it is escaping
-            if (curr == '\\') {
-                currentIndex += 2;
+        while(currentIndex < markdown.length()) {
+            int nextOpenBracket = markdown.indexOf("[", currentIndex);
+            int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
+            int openParen = markdown.indexOf("(", nextCloseBracket);
+            int closeParen = markdown.indexOf(")", openParen);
+
+            if(nextOpenBracket == -1 || nextCloseBracket == -1 || openParen == -1 || closeParen == -1) break;
+
+            if(markdown.substring(openParen, closeParen).indexOf("\n") != -1) {
+                currentIndex = closeParen + 1;
                 continue;
             }
-            //if we are potentially looking at a link with []
-            if (findLink) {
-                // if there arent any other brackets on the bracket tracker
-                if (bracketTracker.isEmpty()) {
-                    if (curr == '(') {
-                        bracketTracker.push(curr);
-                        start = currentIndex;
-                    } else { //something else came after the ] that wasn't (
-                        findLink = false;
-                    }
-                } else {
-                    if (curr == ')') {
-                        end = currentIndex;
-                        if (start + 1 != end) {
-                            toReturn.add(markdown.substring(start + 1, end));
-                        }
-                        bracketTracker.pop();
-                        findLink = false;
-                    }
-                }
-            } else {
-                if (curr == '[') {
-                    bracketTracker.push(curr);
-                } else if (curr == ']') {
-                    if (!bracketTracker.isEmpty()) {
-                        bracketTracker.clear();
-                        findLink = true;
-                    }
-                } else if (curr == '!') {
-                    if (currentIndex < markdown.length() - 1 && markdown.charAt(currentIndex + 1) == '[') {
-                        currentIndex += 2;
-                    }
-                }
-            }
-            // move to next char
-            currentIndex++;
-        }
 
+            if(nextOpenBracket == 0 || markdown.charAt(nextOpenBracket-1) != '!' && markdown.charAt(openParen - 1) == ']') {
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+            }   
+            currentIndex = closeParen + 1;
+        }
         return toReturn;
     }
     public static void main(String[] args) throws IOException {
-        // take in the first command line arg as the file name to be searched for links.
-		Path fileName = Path.of(args[0]);
-        // read the file contents into a string
-	    String contents = Files.readString(fileName);
-        // run getLinks on the contents of the file
+        Path fileName = Path.of(args[0]);
+        String contents = Files.readString(fileName);
         ArrayList<String> links = getLinks(contents);
-        // print out the links that we found
         System.out.println(links);
     }
 }
