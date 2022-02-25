de13f2de95dd2110bab95801b28bc28d73ec5f9e
matty-zee <97648068+matty-zee@users.noreply.github.com>
2022-02-24 17:49:12 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -76,21 +76,21 @@ public class MarkdownParseTest {
     public void testSnippet1() throws IOException {
         String contents= Files.readString(Path.of("./snippet1.md"));
         List<String> expect = List.of("`google.com", "google.com", "ucsd.edu");
-        assertEquals(MarkdownParse.getLinks(contents), expect);
+        assertEquals(expect, MarkdownParse.getLinks(contents));
     }
 
     @Test
     public void testSnippet2() throws IOException {
         String contents= Files.readString(Path.of("./snippet2.md"));
         List<String> expect = List.of("a.com(())", "example.com");
-        assertEquals(MarkdownParse.getLinks(contents), expect);
+        assertEquals(expect, MarkdownParse.getLinks(contents));
     }
 
     @Test
     public void testSnippet3() throws IOException {
         String contents= Files.readString(Path.of("./snippet3.md"));
         List<String> expect = List.of("https://ucsd-cse15l-w22.github.io/");
-        assertEquals(MarkdownParse.getLinks(contents), expect);
+        assertEquals(expect, MarkdownParse.getLinks(contents));
     }
 }
 
